57e6044d2d0c2fa5be2980e1151a52275fbba38d
bool <zhenbeigg@163.com>
2023-11-28 18:59:08 +0800

    修改:请求类

--- src/MqttClient.php
+++ src/MqttClient.php
@@ -4,24 +4,27 @@
  * @author: 布尔
  * @name: mqtt客户端
  * @desc: 介绍
- * @LastEditTime: 2023-07-05 23:08:16
+ * @LastEditTime: 2023-11-28 18:56:30
  */
 declare (strict_types=1);
 namespace Eykj\MqttClient;
 
 use PhpMqtt\Client\MqttClient as PMqttClient;
+use PhpMqtt\Client\ConnectionSettings;
 use function Hyperf\Support\env;
 
 class MqttClient extends  PMqttClient
 {
-
+    protected ConnectionSettings $ConnectionSettings;
     /**
      * @author: 布尔
      * @name: 初始化配置
      * @return {*}
      */    
-    public function __construct(){
-        parent::__construct((string)env("MQTT_HOST",''), (int)env("MQTT_PORT", 1883), env("MQTT_CLIENT_Id",NULL));
+    public function __construct(ConnectionSettings $ConnectionSettings){
+        parent::__construct((string)env("MQTT_HOST",''), (int)env("MQTT_PORT", 1883), env("APP_NAME",NULL)."_".rand(1000,9999));
+        $this->ConnectionSettings = $ConnectionSettings;
+        
     }
 
     /**
@@ -35,10 +38,13 @@ class MqttClient extends  PMqttClient
      */
     public function send(string $topic,string $data,int $qos=2,bool $retain=false)
     {
-        /* 加密钥 */
-        // $param['ticket'] = substr(md5(env('SOCKET_KEY', 'eykjcn2099') . json_encode($param)), 0, 6);
+        /* 设置配置 */
+        $connectionSettings  = ($this->ConnectionSettings)
+        ->setUsername(env('MQTT_USER', ''))
+        ->setPassword(env('MQTT_PASS', ''))
+        ->setKeepAliveInterval(60);
         /*链接*/
-        $this->connect();
+        $this->connect($connectionSettings);
         /*发送*/
         $this->publish($topic,$data,$qos,$retain);
         /*断开*/
