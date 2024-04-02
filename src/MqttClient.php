bbb5d4ead8648db1b346cf9c3f660531619fa6dd
bool <zhenbeigg@163.com>
2024-04-02 11:46:40 +0800

    修改：组件

--- src/MqttClient.php
+++ src/MqttClient.php
@@ -4,9 +4,11 @@
  * @author: 布尔
  * @name: mqtt客户端
  * @desc: 介绍
- * @LastEditTime: 2023-11-28 18:56:30
+ * @LastEditTime: 2024-04-02 11:27:49
  */
-declare (strict_types=1);
+
+declare(strict_types=1);
+
 namespace Eykj\MqttClient;
 
 use PhpMqtt\Client\MqttClient as PMqttClient;
@@ -20,11 +22,11 @@ class MqttClient extends  PMqttClient
      * @author: 布尔
      * @name: 初始化配置
      * @return {*}
-     */    
-    public function __construct(ConnectionSettings $ConnectionSettings){
-        parent::__construct((string)env("MQTT_HOST",''), (int)env("MQTT_PORT", 1883), env("APP_NAME",NULL)."_".rand(1000,9999));
+     */
+    public function __construct(ConnectionSettings $ConnectionSettings)
+    {
+        parent::__construct((string)env("MQTT_HOST", ''), (int)env("MQTT_PORT", 1883), env("APP_NAME", NULL) . "_" . rand(1000, 9999) . "_send");
         $this->ConnectionSettings = $ConnectionSettings;
-        
     }
 
     /**
@@ -36,17 +38,20 @@ class MqttClient extends  PMqttClient
      * @param {bool} $retain 保持
      * @return {*}
      */
-    public function send(string $topic,string $data,int $qos=2,bool $retain=false)
+    public function send(string $topic, string $data, int $qos = 2, bool $retain = false)
     {
         /* 设置配置 */
         $connectionSettings  = ($this->ConnectionSettings)
-        ->setUsername(env('MQTT_USER', ''))
-        ->setPassword(env('MQTT_PASS', ''))
-        ->setKeepAliveInterval(60);
+            ->setUsername(env('MQTT_USER', ''))
+            ->setPassword(env('MQTT_PASS', ''))
+            ->setKeepAliveInterval(60);
+        /* 标记会话状态  0 (false)：客户端在断开连接后希望服务器保留其会话状态。当客户端重新连接时，它将继续使用相同的会话 ID，并且可以访问之前订阅的主题。
+1 (true)：客户端希望服务器在断开连接后清除其会话状态。当客户端重新连接时，它将创建一个新的会话 ID，并且必须重新订阅所有主题。*/
+        $clean_session = true;
         /*链接*/
-        $this->connect($connectionSettings);
+        $this->connect($connectionSettings, $clean_session);
         /*发送*/
-        $this->publish($topic,$data,$qos,$retain);
+        $this->publish($topic, $data, $qos, $retain);
         /*断开*/
         $this->disconnect();
     }
@@ -61,6 +66,6 @@ class MqttClient extends  PMqttClient
     {
         $topic = 'YY2099_' . $param['deviceSn'];
         $data = ['func' => $param['func'], 'data' => $param['data'], 'errmsg' => $param['errmsg'], 'errcode' => $param['errcode']];
-        return $this->send($topic,json_encode($data,320));
+        return $this->send($topic, json_encode($data, 320));
     }
 }
